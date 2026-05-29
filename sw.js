3c271025131377391144203dab212aa0bc99f5ce
Mortlord <martin.staaden@gmail.com>
2026-05-29 15:25:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -16,6 +16,6 @@ self.addEventListener('push', event => {
 self.addEventListener('notificationclick', event => {
   event.notification.close();
   event.waitUntil(
-    clients.openWindow('https://mortlord.github.io/adsb-radar/')
+    clients.openWindow('https://mortlord.github.io/adsb-radar/index.html')
   );
 });
